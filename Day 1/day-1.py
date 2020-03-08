dfcc9e163ab42e811b12a2d98bc2901b2b19ba7f
Matt Chan <chan.598@osu.edu>
2020-03-07 22:24:07 -0500

    Refactor in preparation for part 2

--- Day 1/day-1.py
+++ Day 1/day-1.py
@@ -2,14 +2,22 @@ import math
 
 path = 'day-1-input.txt'
 
+
+def calculate_fuel_required(fuel: int) -> int:
+    return math.floor(fuel/3) - 2
+
+
 file = open(path, 'r')
 
 input_array = file.readlines()
-input_array = list(map(lambda x : int(x.rstrip()), input_array))
 
-input_array = list(map(lambda fuel: math.floor(fuel/3) - 2, input_array))
+# Part 1
+part_1 = list(map(lambda x: int(x.rstrip()), input_array))
+part_1 = list(map(lambda fuel: calculate_fuel_required(fuel), part_1))
 
 part_1_ans = 0
-for fuel in input_array:
-    part_1_ans+= fuel
+for fuel in part_1:
+    part_1_ans += fuel
 print(part_1_ans)
+
+# Part 2
